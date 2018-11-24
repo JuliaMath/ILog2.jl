d90e5a80a40873fe51c8e06a544a7dc474969016
John Lapeyre <jlapeyre@users.noreply.github.com>
2018-11-24 17:37:55 +0100

    Add tests for more types
    	modified:   test/runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,6 +10,15 @@ using Test
           6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]
     @test ilog2.(1:100) == a
     @test ilog2.(Int128(1):100) == a
-
     @test ilog2(big"2"^1000) == 1000
+end
+
+@testset "types" begin
+    n = 64
+    n1 = n + 1
+    ln = 6
+    for T in (Int8, UInt8, Int16, UInt16, Int32, UInt32, Int64, UInt64, Int128, UInt128, BigInt, Float64, BigFloat)
+        @test ilog2(Int32(n)) == ln
+        @test ilog2(Int32(n1)) == ln
+    end
 end
